f2b06873c81a64a6f0c06885f121f62807003e21
EC2 Default User <ec2-user@ip-172-31-17-235.us-east-2.compute.internal>
2018-07-28 12:03:18 +0000

    コマの位置を保持

--- app/controllers/othellos_controller.rb
+++ app/controllers/othellos_controller.rb
@@ -2,8 +2,29 @@ class OthellosController < ApplicationController
   protect_from_forgery except: :reception # receptionアクションを除外
   
   def reception
-    puts "パラメータは"
-    puts params
+    if !params[:board].blank?
+      # オセロの盤面情報を取得
+      board = []
+      index = 0
+      8.times do |i|
+        board[i] = params[:board][index, index+7]
+        index += 8
+      end
+    end
+    
+    # 盤面を表示
+    # ● = 黒
+    # ○ = 白
+    # 空白=駒なし
+    # × = エラー
+    for one_column in board
+      for coma in one_column
+        if one_column == "black"
+          p ""
+      end
+      puts ""
+    end
+    
     @response = 2
     render plain: @response.to_s
   end
